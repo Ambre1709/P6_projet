2a4cf237ff6870a5d0bbba62a264ce9e58d73b61
Ambre Vanneuville <ambre.vanneuville@gmail.com>
2021-01-05 17:16:30 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
+
+mongoose.connect('mongodb+srv://Ambre1709:mdp35@cluster0.ljqsd.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()/*save > enregistre le thing dans la base de donnée*/
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {/*put > modification d'un objet existant*/
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })/*updateOne > mettre à jour/ modifier un object dans la base de donnée*/
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {/*delete > suppression*/
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()/*find > renvoyer un tableau contenant tous les Thing dans notre base de donées*/
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app;
